aadd32574a6c9911fbb6d62efea34c338ee958fa
Nicholas Vaidyanathan <visionary.software.solutions@gmail.com>
2012-04-09 03:21:32 -0700

    finished chapter 16

--- TDDByExample/TDDByExample/src/tddbyexample/Expression.java
+++ TDDByExample/TDDByExample/src/tddbyexample/Expression.java
@@ -3,4 +3,5 @@ package tddbyexample;
 public interface Expression {
     Money reduce(Bank bank, String to);
     Expression plus(Expression addend);
+    Expression times(int multiplier);
 }
--- TDDByExample/TDDByExample/src/tddbyexample/Sum.java
+++ TDDByExample/TDDByExample/src/tddbyexample/Sum.java
@@ -25,6 +25,10 @@ class Sum implements Expression {
     
     @Override
     public Expression plus(Expression addend){
-        return null;
+        return new Sum(this, addend);
+    }
+
+    public Expression times(int multiplier) {
+        return new Sum(augend.times(multiplier), addend.times(multiplier));
     }
 }
