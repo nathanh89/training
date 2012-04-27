0d1054b50ce759fb2ba3b325adacfbef43709080
Nicholas Vaidyanathan <visionary.software.solutions@gmail.com>
2012-04-26 22:50:28 -0700

    iterating

--- WorkingEffectivelyWithLegacyCode/TemplateRedefinition/src/com/visionarysoftwaresolutions/legacycode/templateredefinition/original/Walk.java
+++
@@ -1,9 +0,0 @@
-package com.visionarysoftwaresolutions.legacycode.templateredefinition.original;
-
-public class Walk {
-    public void doIt(){
-        for(int i =0; i < 10000; i++){
-            System.out.println("Wheeeeeeee!");
-        }
-    }
-}
