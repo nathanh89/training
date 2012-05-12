6933a8356476b98089b39e262a72ae33261dc75d
nathanh89 <nathanuhutcheson@gmail.com>
2012-05-12 15:08:05 -0700

    I'm tired of typing this commit message. But fine. I extracted two classes from Cooler, but have it set up so Cooler is inheriting from StorageUnit inheriting from PowerableUnit, which is BS because a StorageUnit is not necessarily powerable. But the code green bars now, and my interfaces are visible

---
+++ JavaLearning/AutomaticRefiller/src/com/visionarysoftwaresolutions/refillable/StorageUnitInterface.java
@@ -0,0 +1,11 @@
+package com.visionarysoftwaresolutions.refillable;
+
+public interface StorageUnitInterface {
+
+	public boolean isDoorOpen();
+
+	public void operateDoor();
+
+	public void setDoorOpen(boolean b);
+
+}
