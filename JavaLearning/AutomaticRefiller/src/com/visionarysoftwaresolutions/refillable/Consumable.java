0179e16f65178eed662f518b3ebb3ededa96d08b
nathanh89 <nathanuhutcheson@gmail.com>
2012-05-12 12:41:17 -0700

    Extracted Consumable from Bottle, took out all properties relating to pricing of the Bottle

---
+++ JavaLearning/AutomaticRefiller/src/com/visionarysoftwaresolutions/refillable/Consumable.java
@@ -0,0 +1,20 @@
+package com.visionarysoftwaresolutions.refillable;
+
+
+public class Consumable {
+
+	public double price;
+
+	public Consumable() {
+		super();
+	}
+
+	public void setPrice(double beveragePrice) {
+	    this.price = beveragePrice;
+	}
+
+	public double getPrice() {
+	    return price;
+	}
+
+}
