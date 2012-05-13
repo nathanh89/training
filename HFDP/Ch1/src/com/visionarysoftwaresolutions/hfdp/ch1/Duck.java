9ead4306402bd743b31b5c606774df841338733f
nathanh89 <nathanuhutcheson@gmail.com>
2012-05-13 07:47:21 -0700

    Set up HFDP folder/project, first commit on that

---
+++ HFDP/Ch1/src/com/visionarysoftwaresolutions/hfdp/ch1/Duck.java
@@ -0,0 +1,20 @@
+package com.visionarysoftwaresolutions.hfdp.ch1;
+
+public abstract class Duck {
+	
+	
+	public void quack(){
+		
+	}
+	
+	public void swim(){
+		
+	}
+	
+	public void fly(){
+		
+	}
+	
+	abstract void display();
+
+}
