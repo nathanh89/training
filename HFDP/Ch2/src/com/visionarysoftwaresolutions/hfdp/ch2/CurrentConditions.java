f49c7eba8aead6862477a7fe86e9ee0acf9a2d11
nathanh89 <nathanuhutcheson@gmail.com>
2012-05-15 12:57:37 -0700

    I've done something, and I think it is cool. Still in Chapter 2. Sleep-deprived brain...splintering...

---
+++ HFDP/Ch2/src/com/visionarysoftwaresolutions/hfdp/ch2/CurrentConditions.java
@@ -0,0 +1,31 @@
+package com.visionarysoftwaresolutions.hfdp.ch2;
+
+public class CurrentConditions implements ObservingDisplay{
+	
+	private float temperature;
+	private float humidity;
+	private Subject weatherData;
+	
+	public CurrentConditions(Subject weatherData){
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+	}
+	
+	public CurrentConditions(){
+		
+	}
+
+	@Override
+	public void update(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		display();
+	}
+
+	@Override
+	public void display() {
+		System.out.println("Current conditions: " + temperature + 
+				"F degrees and " + humidity + "% humidity");
+	}
+
+}
