a53d2a7ffa91432fffb2356d49e6ad4d9883d809
nathanh89 <nathanuhutcheson@gmail.com>
2012-05-16 15:30:37 -0700

    Added empty test cases for all classes in hfdp ch1, seperated into appropriate packages

---
+++ HFDP/Ch1/Ch1.TDDStyle/tests/com/visionarysoftwaresolutions/hfdp/ch1/tddstyle/flight/FlyNoWayTests.java
@@ -0,0 +1,14 @@
+package com.visionarysoftwaresolutions.hfdp.ch1.tddstyle.flight;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlyNoWayTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ HFDP/Ch1/Ch1.TDDStyle/tests/com/visionarysoftwaresolutions/hfdp/ch1/tddstyle/simulator/IntegrationTestsMiniDuckSimulator.java
@@ -0,0 +1,14 @@
+package com.visionarysoftwaresolutions.hfdp.ch1.tddstyle.simulator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IntegrationTestsMiniDuckSimulator {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
