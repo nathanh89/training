c8b36d15f6950c2919e6762a5488f4bbb33ee8a6
nathanh89 <nathanuhutcheson@gmail.com>
2012-05-16 18:09:50 -0700

    Wrote all test for Mallard class, fixed the display method to return a string, as I missed that earlier

---
+++ HFDP/Ch1/.metadata/.plugins/org.eclipse.core.resources/.history/d6/302a13b8bb9f0011175fbd9a3a7e9265
@@ -0,0 +1,10 @@
+package com.visionarysoftwaresolutions.hfdp.ch1.tddstyle.ducks;
+
+public class RubberDuck extends Duck{
+	
+	@Override
+	public void display(){
+		System.out.println("Burt's always stealing my bathtime with Ernie..." +
+				"OH HAI! I'm a rubber duckie.");
+	}
+}
--- HFDP/Ch1/Ch1.TDDStyle/src/com/visionarysoftwaresolutions/hfdp/ch1/tddstyle/ducks/RubberDuck.java
+++ HFDP/Ch1/Ch1.TDDStyle/src/com/visionarysoftwaresolutions/hfdp/ch1/tddstyle/ducks/RubberDuck.java
@@ -3,8 +3,7 @@ package com.visionarysoftwaresolutions.hfdp.ch1.tddstyle.ducks;
 public class RubberDuck extends Duck{
 	
 	@Override
-	public void display(){
-		System.out.println("Burt's always stealing my bathtime with Ernie..." +
-				"OH HAI! I'm a rubber duckie.");
+	public String display(){
+		return "Burt's always invading my bathtime with Ernie...";
 	}
 }
