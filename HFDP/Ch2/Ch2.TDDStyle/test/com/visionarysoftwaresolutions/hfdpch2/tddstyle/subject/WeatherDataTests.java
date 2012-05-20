7c1be33c31751cec8ee6671838b2d6da597223c1
nathanh89 <nathanuhutcheson@gmail.com>
2012-05-20 16:53:10 -0700

    Stopping point for CH2 HFDP. Got enough functionality in my extension of WeatherStation to prove it would work if I implemented it further, and enough tests to document pertinent functionality

--- HFDP/Ch2/Ch2.TDDStyle/test/com/visionarysoftwaresolutions/hfdpch2/tddstyle/subject/WeatherDataTests.java
+++ HFDP/Ch2/Ch2.TDDStyle/test/com/visionarysoftwaresolutions/hfdpch2/tddstyle/subject/WeatherDataTests.java
@@ -1,14 +1,14 @@
 package com.visionarysoftwaresolutions.hfdpch2.tddstyle.subject;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class WeatherDataTests {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
+//I've included this testCase file, but if the WeatherData class is "provided"
+//as it is supposed to be in this example, then so would the tests.
+//Well, I would hope that they are. Anyway, WeatherData is more of a simulation
+//and it's functionality is demonstrated by the fact that the observer classes work
+//at this point.
+	
 }
