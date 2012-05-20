1899232c3cbfaf630bf4bde0c84ab129e9a83c2e
nathanh89 <nathanuhutcheson@gmail.com>
2012-05-20 15:40:34 -0700

    Set Up for final serieries of tests to be written

---
+++ HFDP/Ch2/Ch2.TDDStyle/test/com/visionarysoftwaresolutions/hfdpch2/tddstyle/observers/HeatIndexTests.java
@@ -0,0 +1,14 @@
+package com.visionarysoftwaresolutions.hfdpch2.tddstyle.observers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HeatIndexTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ HFDP/Ch2/Ch2.TDDStyle/test/com/visionarysoftwaresolutions/hfdpch2/tddstyle/subject/WeatherDataTests.java
@@ -0,0 +1,14 @@
+package com.visionarysoftwaresolutions.hfdpch2.tddstyle.subject;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WeatherDataTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
