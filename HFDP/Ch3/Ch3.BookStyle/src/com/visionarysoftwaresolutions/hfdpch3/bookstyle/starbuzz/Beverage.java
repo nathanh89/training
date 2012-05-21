b97587cfa0c1d03089a79cd2ebd6e57020c38c03
nathanh89 <nathanuhutcheson@gmail.com>
2012-05-21 16:08:07 -0700

    Done with Starbuzz exath Starbuzz example (book style) of HFDP Ch3.

---
+++ HFDP/Ch3/Ch3.BookStyle/src/com/visionarysoftwaresolutions/hfdpch3/bookstyle/starbuzz/Beverage.java
@@ -0,0 +1,13 @@
+package com.visionarysoftwaresolutions.hfdpch3.bookstyle.starbuzz;
+
+public abstract class Beverage {
+	
+	String description = "Unknown Beverage";
+	
+	public String getDescription(){
+		return description;
+	}
+	
+	public abstract double cost();
+
+}
---
+++ HFDP/Ch3/Ch3.BookStyle/src/com/visionarysoftwaresolutions/hfdpch3/bookstyle/starbuzz/Decaf.java
@@ -0,0 +1,12 @@
+package com.visionarysoftwaresolutions.hfdpch3.bookstyle.starbuzz;
+
+public class Decaf extends Beverage{
+
+	public Decaf(){
+		description = "Decaf Coffee";
+	}
+	
+	public double cost(){
+		return 1.05;
+	}
+}
