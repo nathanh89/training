8283f16da47c8b63ffc4cbc437c556d5800e3cea
nathanh89 <nathanuhutcheson@gmail.com>
2012-05-23 10:07:01 -0700

    Defined Command Pattern

---
+++ HFDP/Ch6/Ch6.Bookstyle/src/com/visionarysoftwaresolutions/hfdpch6/bookstyle/simpleremote/Command.java
@@ -0,0 +1,6 @@
+package com.visionarysoftwaresolutions.hfdpch6.bookstyle.simpleremote;
+
+public interface Command {
+
+	public void execute();
+}
---
+++ HFDP/Ch6/Ch6.Bookstyle/src/com/visionarysoftwaresolutions/hfdpch6/bookstyle/simpleremote/Light.java
@@ -0,0 +1,15 @@
+package com.visionarysoftwaresolutions.hfdpch6.bookstyle.simpleremote;
+
+public class Light {
+
+	public Light() {
+	}
+
+	public void on() {
+		System.out.println("Light is on");
+	}
+
+	public void off() {
+		System.out.println("Light is off");
+	}
+}
