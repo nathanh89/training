00f296eef3ffa373f3a38769d3b959f77530fe79
Nathan Hutcheson <nathanuhutcheson@gmail.com>
2012-11-08 04:05:57 -0700

    Removed AutomaticRefiller and BudgetBoss to own repos

--- AutomaticRefiller/src/com/visionarysoftwaresolutions/refillable/interfaces/PowerableStorageUnit.java
+++
@@ -1,8 +0,0 @@
-package com.visionarysoftwaresolutions.refillable.interfaces;
-
-import com.visionarysoftwaresolutions.ordering.Storable;
-
-
-public interface PowerableStorageUnit extends Storable, Powerable {
-
-}
